35d766690fbd9c29770ee518228ecae3a81dfc3b
Larisa Gonchar <larisa_gonchar@epam.com>
2023-07-28 15:00:20 +0200

    feat: add swagger functionality

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { load } from 'js-yaml';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = load(
+    readFileSync('./doc/api.yaml', 'utf8'),
+  ) as OpenAPIObject;
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
