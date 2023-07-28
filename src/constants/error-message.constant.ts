ec37b5be96419d80bfc01067f50bf9615646eecc
Larisa Gonchar <larisa_gonchar@epam.com>
2023-07-28 14:39:32 +0200

    feat: add constants

---
+++ src/constants/error-message.constant.ts
@@ -0,0 +1,6 @@
+export enum ErrorMessage {
+  ID_NOT_EXIST = "Item with such id doesn't exist in database",
+  NOT_ALL_FIELDS = "Body doesn't contain all required fields",
+  ID_NOT_VALID = "isn't valid id",
+  INVALID_TYPE_OF_FIELDS = 'Invalid type of fields',
+}
