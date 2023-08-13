cf864dba5c246df6f1cf59f5524f7f87f926a52a
Larisa Gonchar <larisa_gonchar@epam.com>
2023-08-13 14:42:11 +0200

    feat: delete module

--- src/api/modules/api-artists/artist.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ApiArtistService } from './artist.service';
-
-@Module({
-  providers: [ApiArtistService],
-  exports: [ApiArtistService],
-})
-export class ApiArtistModule {}
